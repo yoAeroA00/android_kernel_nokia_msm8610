7ebb3b6d71c166b775b247a378f81ceba5f56527
yoAeroA00 <aditya.raghuvanshi2000@gmail.com>
2017-10-06 20:12:22 +0530

    Stability Update

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -71,11 +71,11 @@ static struct mutex gov_lock;
 static unsigned int hispeed_freq = DEFAULT_HISPEED_FREQ;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 95
+#define DEFAULT_GO_HISPEED_LOAD 85
 static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 
 /* Go to lowest speed when CPU load at or below this value. */
-#define DEFAULT_GO_LOWSPEED_LOAD 10
+#define DEFAULT_GO_LOWSPEED_LOAD 15
 static unsigned long go_lowspeed_load = DEFAULT_GO_LOWSPEED_LOAD;
 
 /* Target load.  Lower values result in higher CPU speeds. */
--- drivers/cpufreq/cpufreq_interactive_pro.c
+++ drivers/cpufreq/cpufreq_interactive_pro.c
@@ -71,7 +71,7 @@ static struct mutex gov_lock;
 static unsigned int hispeed_freq = 998400;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 99
+#define DEFAULT_GO_HISPEED_LOAD 98
 static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 
 /* Sampling down factor to be applied to min_sample_time at max freq */
